708a48f3e01ee623eb3fdc4fb7fa059ed5ab2615
GabrielleHoyte <gabriellehoyte@gmail.com>
2022-05-30 20:03:48 -0400

    Answer

--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
 var thumbUp = document.getElementsByClassName("fa-thumbs-up");
+var thumbDown = document.getElementsByClassName("fa-thumbs-down");
 var trash = document.getElementsByClassName("fa-trash");
 
 Array.from(thumbUp).forEach(function(element) {
@@ -25,21 +26,51 @@ Array.from(thumbUp).forEach(function(element) {
       });
 });
 
+Array.from(thumbDown).forEach(function(element) {
+  element.addEventListener('click', function(){
+    const name = this.parentNode.parentNode.childNodes[1].innerText
+    const msg = this.parentNode.parentNode.childNodes[3].innerText
+    const thumbDown = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+    fetch('dislike', {
+      method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'name': name,
+        'msg': msg,
+        'thumbUp':thumbDown
+      })
+    })
+    .then(response => {
+      if (response.ok) return response.json()
+    })
+    .then(data => {
+      console.log(data)
+      window.location.reload(true)
+    })
+  });
+});
+
 Array.from(trash).forEach(function(element) {
       element.addEventListener('click', function(){
         const name = this.parentNode.parentNode.childNodes[1].innerText
         const msg = this.parentNode.parentNode.childNodes[3].innerText
         fetch('messages', {
           method: 'delete',
-          headers: {
-            'Content-Type': 'application/json'
-          },
+          headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
             'name': name,
             'msg': msg
           })
         }).then(function (response) {
-          window.location.reload()
+          return response.json()
+          console.log(response)
+        })
+        .then(data => {
+          const h2 = document.querySelector('#message')
+          h2.innerText = data.message
+          setTimeout(() => {
+            window.location.reload()
+          }, "1000")
         })
       });
 });
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require('express') //loads the module
+const app = express() //executes the module, starts express
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 
 var db, collection;
 
-const url = "mongodb+srv://demo:demo@cluster0-q2ojb.mongodb.net/test?retryWrites=true";
+const url = 'mongodb+srv://GabrielleHoyte:Password0@cluster0.ioxl7.mongodb.net/?retryWrites=true&w=majority'
 const dbName = "demo";
 
 app.listen(3000, () => {
@@ -14,35 +14,50 @@ app.listen(3000, () => {
             throw error;
         }
         db = client.db(dbName);
+        collection = db.collection('messages')
         console.log("Connected to `" + dbName + "`!");
     });
 });
 
 app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true})) //middle-ware setup
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  db.collection('messages').find().toArray((err, result) => {
+app.get('/', (req, res) => { //requesting from the server to render the index.ejs
+  collection.find().toArray((err, result) => {
     if (err) return console.log(err)
-    res.render('index.ejs', {messages: result})
+    res.render('index.ejs', {messages: result}) //returns whatever messages are already stored in the db
   })
 })
 
-app.post('/messages', (req, res) => {
-  db.collection('messages').insertOne({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
+app.post('/messages', (req, res) => { //set up a handler to handle post requests to /messages
+  collection.insertOne({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
     if (err) return console.log(err)
     console.log('saved to database')
     res.redirect('/')
   })
 })
 
-app.put('/messages', (req, res) => {
-  db.collection('messages')
-  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+app.put('/messages', (req, res) => { //handler to update the posts in /messages
+  collection.findOneAndUpdate({name: req.body.name, msg: req.body.msg},{
     $set: {
       thumbUp:req.body.thumbUp + 1
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result) //the response to the client
+  })
+})
+
+app.put('/dislike', (req, res) => { //different uri to run the thumb down function 
+  collection
+  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+    $set: {
+      thumbUp:req.body.thumbUp - 1 //only other difference is the '-' as we are referencing the same thumbs up number
     }
   }, {
     sort: {_id: -1},
@@ -54,8 +69,10 @@ app.put('/messages', (req, res) => {
 })
 
 app.delete('/messages', (req, res) => {
-  db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
+  let message = {message: 'Message deleted!'}
+  collection.findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
     if (err) return res.send(500, err)
-    res.send('Message deleted!')
+    res.send(message)
+      
   })
 })
